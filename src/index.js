289af8ef36b1266b95ea9f25cabbcbb08ae10a41
kallyas <kallyasmedia@gmail.com>
2022-03-28 22:54:46 +0300

    final

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const ORIGIN = process.env.NODE_ENV == 'production' ? process.env.ORIGIN_PROD : 'http://localhost:3000'
+
+const corsOptions = {
+    origin: ORIGIN,
+    credentials: false,
+    optionsSuccessStatus: 200
+};
 
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
